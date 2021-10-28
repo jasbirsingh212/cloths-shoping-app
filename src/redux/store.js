8fb30b254c472fe4a572329a778dce18aa625fa4
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2021-10-28 16:42:00 +0530

    error resolved

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,7 @@ const middleWares = [logger];
 
 export const  store = createStore(rootReducer,compose(
     // @ts-ignore
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
      applyMiddleware(
    ...middleWares
 )),);
