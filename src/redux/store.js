5ac0da367ec58ed6cae3e9f1c40c8eabe25011a1
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2021-11-09 15:14:58 +0530

    resolved async action object error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,28 @@
 // @ts-nocheck
 import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer  from "./root-reducer";
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import rootReducer from "./root-reducer";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 import { persistStore } from "redux-persist";
 
-const middleWares = [thunk]
+const middleWares = [thunk];
 
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   // @ts-ignore
-  middleWares.push(logger)
+  middleWares.push(logger);
 }
 
-export const  store = createStore(rootReducer,compose(
+export const store = createStore(
+  rootReducer,
+  compose(
     // @ts-ignore
-    // window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : noop => noop,
+    applyMiddleware(...middleWares),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (noop) => noop
     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-     applyMiddleware(
-   ...middleWares,
-)),);
+  )
+);
 
 export const persistor = persistStore(store);
 
