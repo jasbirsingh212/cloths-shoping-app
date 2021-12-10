6c286e5ffb7ce205859424f9759021ad6bd0031f
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2021-12-10 10:19:00 +0530

    debugg stripe

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ console.log(req)
     const body = {
         source: req.body.token.id,
         amount: req.body.amount,
-        currency: 'usd'
+        currency: process.env.NODE_ENV !== 'production' ? 'inr': 'used'
     };
 
 
