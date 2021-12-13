345eb8c7d03a0b11bfe22603b2cb5d4939d272a2
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2021-12-13 12:02:15 +0530

    added pwa

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // @ts-ignore
 import { store, persistor} from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -22,7 +23,16 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const enforce = require('express-sslify');
 
 if(process.env.NODE_ENV !== 'production'){
 require('dotenv').config()
@@ -12,17 +13,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
-console.log(process.env.SECRATE_STRIPE_API_KEY)
 
 const port = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === 'production'){
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client-side/build')))
     app.get('*', (req, res) =>{
         res.sendFile(path.join(__dirname, 'client-side/build', 'index.html'))
     })
 }
 
+
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'service-worker.js'));
+})
 
 app.listen(port, (error) => {
     if(error) throw(error);
